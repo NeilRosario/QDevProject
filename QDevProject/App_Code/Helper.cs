1283a1491ede63253c0289380c779cd44744c72b
NeilRosario <neillawrence.rosario@benilde.edu.ph>
2019-02-03 02:40:55 +0800

    #2

---
+++ QDevProject/App_Code/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace QDevProject.App_Code
+{
+    public class Helper
+    {
+        public static string GetConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+        }
+    }
+}
